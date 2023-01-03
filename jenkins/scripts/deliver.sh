04c9bd7d051760e6ff1935431b6f4a1e51069b2b
wisnu_wiranata <wisnu.wiranata@ctcorpdigital.com>
2023-01-03 15:50:36 +0700

    add deploy stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,7 +5,6 @@ echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
-npm audit fix --force
 set -x
 npm run build
 set +x
